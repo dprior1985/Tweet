b5272fafd5211105b6ef2e86484108349a999ac7
dprior1985 <dannyprior1985@gmail.com>
2016-10-11 23:06:34 +0100

    temp option
    add temp option from DB

--- SillyTweet.py
+++ SillyTweet.py
@@ -3,7 +3,7 @@ import sys
 from twython import Twython
 import os
 import datetime
-
+import MySQLdb
 
 CONSUMER_KEY = '## PERSONAL KEY ##'
 CONSUMER_SECRET = '## PERSONAL KEY ##'
@@ -15,6 +15,13 @@ api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
 task = status=sys.argv[1]
 message = status=sys.argv[2]
+
+# Open database connection
+db = MySQLdb.connect("localhost","danny","danny123","MYGARDEN" )
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+vWeatherAPITempData = "404 error";
+vTempsensor2 = "404 error";
 
 if (task == 'CPU'):
         cmd = '/opt/vc/bin/vcgencmd measure_temp'
@@ -35,3 +42,19 @@ if (task =='chart3'):
 if (task =='chart4'):
         photo = open('/var/www/Graph4.png','rb')
         api.update_status_with_media(media=photo, status='Inside Temp Chart all time- '+str(datetime.datetime.now()))
+if (task = 'tmep'):
+	cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'temp sensor 2' ;" )
+	for row in cursor.fetchall():
+
+		vTempsensor2 = (row[0])	
+	
+	api.update_status(status='Internal Temp - '+str(vTempsensor2)+'C - '+str(datetime.datetime.now()))
+	
+	cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'Weather API' and LogDescription = 'Temp String' ;" )
+	for row in cursor.fetchall():
+
+		vWeatherAPITempData = (row[0])
+	
+	api.update_status(status='API - Bexley Temp - '+str(vWeatherAPITempData)+' - '+str(datetime.datetime.now()))
+	
+
