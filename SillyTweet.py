d564c368dc7f3f8a91b7ddaa888a65ba6fd61757
dprior1985 <dannyprior1985@gmail.com>
2016-10-12 18:48:12 +0100

    Update SillyTweet.py
    Include weather information from API weather

--- SillyTweet.py
+++ SillyTweet.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
 import sys
 from twython import Twython
 import os
 import datetime
 import MySQLdb
 
-CONSUMER_KEY = '## PERSONAL KEY ##'
-CONSUMER_SECRET = '## PERSONAL KEY ##'
-ACCESS_KEY = '## PERSONAL KEY ##'
-ACCESS_SECRET = '## PERSONAL KEY ##'
-
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
@@ -20,8 +18,10 @@ message = status=sys.argv[2]
 db = MySQLdb.connect("localhost","danny","danny123","MYGARDEN" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
-vWeatherAPITempData = "404 error";
-vTempsensor2 = "404 error";
+vWeatherAPITempData = "404 error ";
+vTempsensor2 = "404 error ";
+vWeatherAPIWeatherData = "404 error ";
+
 
 if (task == 'CPU'):
         cmd = '/opt/vc/bin/vcgencmd measure_temp'
@@ -38,23 +38,26 @@ if (task =='chart2'):
         api.update_status_with_media(media=photo, status='Inside Temp Chart - '+str(datetime.datetime.now()))
 if (task =='chart3'):
         photo = open('/var/www/Graph3.png','rb')
-        api.update_status_with_media(media=photo, status='Outside Temp Chart all time - '+str(datetime.datetime.now()))
+        api.update_status_with_media(media=photo, status='Outside Temp Chart all time -- '+str(datetime.datetime.now()))
 if (task =='chart4'):
         photo = open('/var/www/Graph4.png','rb')
-        api.update_status_with_media(media=photo, status='Inside Temp Chart all time- '+str(datetime.datetime.now()))
-if (task = 'tmep'):
-	cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'temp sensor 2' ;" )
-	for row in cursor.fetchall():
+        api.update_status_with_media(media=photo, status='Inside Temp Chart all time -- '+str(datetime.datetime.now()))
+if (task == 'temp'):
+        cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'temp sensor 2' ;" )
+        for row in cursor.fetchall():
 
-		vTempsensor2 = (row[0])	
-	
-	api.update_status(status='Internal Temp - '+str(vTempsensor2)+'C - '+str(datetime.datetime.now()))
-	
-	cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'Weather API' and LogDescription = 'Temp String' ;" )
-	for row in cursor.fetchall():
+                vTempsensor2 = (row[0])
 
-		vWeatherAPITempData = (row[0])
-	
-	api.update_status(status='API - Bexley Temp - '+str(vWeatherAPITempData)+' - '+str(datetime.datetime.now()))
-	
+        api.update_status(status='Internal Temp - '+str(vTempsensor2)+'C -- '+str(datetime.datetime.now()))
 
+        cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'Weather API' and LogDescription = 'Weather' ;" )
+        for row in cursor.fetchall():
+
+                vWeatherAPIWeatherData = (row[0])
+
+        cursor.execute("select SaveData from ControlLog where RunNumberId = (select max(RunNumberId) from RunNumber) and ActionName = 'Weather API' and LogDescription = 'Temp String' ;" )
+        for row in cursor.fetchall():
+
+                vWeatherAPITempData = (row[0])
+
+        api.update_status(status='API - Temp - '+str(vWeatherAPITempData)+' -- Weather -'+ vWeatherAPIWeatherData +' -- '+str(datetime.datetime.now()))
